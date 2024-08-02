5cf17ac3bf2cea1824e99579a14c9e5ea16ca9a9
Ronoaldo JLP <ronoaldo@gmail.com>
2024-08-02 12:21:41 -0300

    feat: initial version of linux-guru program.
    Uses the Google AI Studio API keys that has a free quota.

---
+++ README.md
@@ -0,0 +1,51 @@
+## linux-guru: seu assistente pessoal de IA para o Terminal 
+
+Este projeto utiliza o poder do Gemini AI, um modelo de linguagem avançado do
+Google, para fornecer funcionalidades inteligentes. Para usar o programa, siga
+os passos abaixo:
+
+### Instalação
+
+1. **Pré-requisitos:**
+   - Go (versão 1.18 ou superior) instalado em seu sistema.
+   - Conta Google pessoal para acessar o Google AI Studio.
+
+2. **Instalação do Programa:**
+   - Abra seu terminal e execute o seguinte comando:
+```bash
+go install
+github.com/ronoaldo/linux-guru/cmd/linux-guru@latest
+```
+   - Isso irá baixar e instalar o programa `linux-guru` em seu sistema.
+
+### Obtenção e Configuração da Chave de API do Gemini (Google AI Studio)
+
+1. **Acesse o Google AI Studio:**
+   - Faça login em sua conta Google pessoal.
+   - Acesse o site do Google AI Studio:
+     [https://aistudio.google.com/app/apikey](https://aistudio.google.com/app/apikey)
+
+2. **Crie uma Nova Chave de API:**
+   - Clique no botão "Get API Key" (Obter Chave de API).
+   - Siga as instruções para criar uma nova chave de API.
+   - Uma chave de API única será gerada para você.
+
+3. **Configure a Variável de Ambiente:**
+   - Copie a chave de API obtida.
+   - Abra seu terminal e execute o seguinte comando, substituindo
+     `<SUA_CHAVE_DE_API>` pela chave que você copiou:
+```bash export
+GEMINI_API_KEY=<SUA_CHAVE_DE_API>
+```
+
+### Uso do Programa
+
+Agora que o programa está instalado e a variável de ambiente `GEMINI_API_KEY`
+está configurada, você pode executar o programa `linux-guru` em seu terminal
+para começar a utilizá-lo. Consulte a documentação específica do projeto para
+saber como interagir com o Gemini AI e aproveitar seus recursos.
+
+**Importante: na versão gratuita, os dados enviados são utilizados pelo Google
+para aprimorar os modelos. Caso queira, ative o faturamento e faça upgrade
+para uma conta paga onde seus dados são privados.**
+
---
+++ cmd/linux-guru/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	client := newClient(ctx)
+	defer client.Close()
+
+	model := setupModel(client)
+
+	userPrompt := strings.Join(flag.Args(), " ")
+	userPrompt = strings.TrimSpace(userPrompt)
+	if userPrompt == "" {
+		userPrompt = "Em que você pode me ajudar?"
+	}
+
+	resp, err := model.GenerateContent(ctx, genai.Text(userPrompt))
+	if err != nil {
+		log.Fatalf("Error sending message: %v\n", err)
+	}
+
+	for _, part := range resp.Candidates[0].Content.Parts {
+		fmt.Printf("%v\n", part)
+	}
+}
+
+func newClient(ctx context.Context) *genai.Client {
+	apiKey, found := os.LookupEnv("GEMINI_API_KEY")
+	if !found {
+		log.Fatal("linux-guru: environment variable GEMINI_API_KEY not set. " +
+			"Setup a free KEY at https://aistudio.google.com/app/apikey")
+	}
+	option := option.WithAPIKey(apiKey)
+
+	client, err := genai.NewClient(ctx, option)
+	if err != nil {
+		log.Fatalf("linux-guru: error creating client: %v", err)
+	}
+	return client
+}
+
+var systemPrompt = `Você é um assistente de usuários de terminais de comandos do Linux.
+Especializado em várias distribuições de Linux e GNU/Linux.
+Você só responde a perguntas sobre Linux, como instalação de pacotes,
+origem e criação do Linux, programação utilizando Shell Script.
+Suas respostas devem ser simples.
+Evite respostas muito prolixas.
+Para qualquer outra pergunta você responde:
+"Não conheço sobre este assunto! Tente uma pergunta sobre Linux".
+Se a resposta tiver algum código fonte ou comando, incluir no final:
+"Atenção: sempre revise o código e comandos antes de executá-los!"`
+
+func setupModel(client *genai.Client) *genai.GenerativeModel {
+	model := client.GenerativeModel("gemini-1.5-pro-exp-0801")
+
+	model.SetTemperature(1)
+	model.SetTopK(64)
+	model.SetTopP(0.95)
+	model.SetMaxOutputTokens(8192)
+	model.ResponseMIMEType = "text/plain"
+	model.SystemInstruction = &genai.Content{
+		Parts: []genai.Part{genai.Text(systemPrompt)},
+	}
+
+	return model
+}
