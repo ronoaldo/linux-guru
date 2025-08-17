01f3c4640c70ddb139beba29cf5cc84f529ceb34
Ronoaldo JLP <ronoaldo@gmail.com>
2025-08-17 19:59:19 -0300

    chore: updated to google.golang.org/genai.
    * Added vendor directory
    * Updated application to use defaults for TopP and TopK
    * Used the new API to explicitly configure the Gemini API

--- cmd/linux-guru/main.go
+++ cmd/linux-guru/main.go
@@ -8,49 +8,10 @@ import (
 	"os"
 	"strings"
 
-	"github.com/google/generative-ai-go/genai"
-	"google.golang.org/api/option"
+	"google.golang.org/genai"
 )
 
-func main() {
-	flag.Parse()
-	ctx := context.Background()
-
-	client := newClient(ctx)
-	defer client.Close()
-
-	model := setupModel(client)
-
-	userPrompt := strings.Join(flag.Args(), " ")
-	userPrompt = strings.TrimSpace(userPrompt)
-	if userPrompt == "" {
-		userPrompt = "Em que você pode me ajudar?"
-	}
-
-	resp, err := model.GenerateContent(ctx, genai.Text(userPrompt))
-	if err != nil {
-		log.Fatalf("Error sending message: %v\n", err)
-	}
-
-	for _, part := range resp.Candidates[0].Content.Parts {
-		fmt.Printf("%v\n", part)
-	}
-}
-
-func newClient(ctx context.Context) *genai.Client {
-	apiKey, found := os.LookupEnv("GEMINI_API_KEY")
-	if !found {
-		log.Fatal("linux-guru: environment variable GEMINI_API_KEY not set. " +
-			"Setup a free KEY at https://aistudio.google.com/app/apikey")
-	}
-	option := option.WithAPIKey(apiKey)
-
-	client, err := genai.NewClient(ctx, option)
-	if err != nil {
-		log.Fatalf("linux-guru: error creating client: %v", err)
-	}
-	return client
-}
+var model = flag.String("model", "gemini-2.5-flash", "The model to use when calling")
 
 var systemPrompt = `Você é um assistente de usuários de terminais de comandos do Linux.
 Especializado em várias distribuições de Linux e GNU/Linux.
@@ -63,17 +24,49 @@ Para qualquer outra pergunta você responde:
 Se a resposta tiver algum código fonte ou comando, incluir no final:
 "Atenção: sempre revise o código e comandos antes de executá-los!"`
 
-func setupModel(client *genai.Client) *genai.GenerativeModel {
-	model := client.GenerativeModel("gemini-2.5-flash")
+var config = &genai.GenerateContentConfig{
+	Temperature:       genai.Ptr[float32](0.2),
+	MaxOutputTokens:   8192,
+	ResponseMIMEType:  "text/plain",
+	SystemInstruction: genai.Text(systemPrompt)[0],
+}
 
-	model.SetTemperature(0.2)
-	model.SetTopK(64)
-	model.SetTopP(0.95)
-	model.SetMaxOutputTokens(8192)
-	model.ResponseMIMEType = "text/plain"
-	model.SystemInstruction = &genai.Content{
-		Parts: []genai.Part{genai.Text(systemPrompt)},
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	client := newClient(ctx)
+
+	userPrompt := strings.Join(flag.Args(), " ")
+	userPrompt = strings.TrimSpace(userPrompt)
+	if userPrompt == "" {
+		userPrompt = "Em que você pode me ajudar?"
 	}
 
-	return model
+	resp, err := client.Models.GenerateContent(ctx, *model, genai.Text(userPrompt), config)
+	if err != nil {
+		log.Fatalf("Error sending message: %v\n", err)
+	}
+
+	for _, part := range resp.Candidates[0].Content.Parts {
+		fmt.Printf("%v\n", part.Text)
+	}
+}
+
+func newClient(ctx context.Context) *genai.Client {
+	apiKey, found := os.LookupEnv("GEMINI_API_KEY")
+	if !found {
+		log.Fatal("linux-guru: environment variable GEMINI_API_KEY not set. " +
+			"Setup a free KEY at https://aistudio.google.com/app/apikey")
+	}
+	config := &genai.ClientConfig{
+		APIKey:  apiKey,
+		Backend: genai.BackendGeminiAPI,
+	}
+
+	client, err := genai.NewClient(ctx, config)
+	if err != nil {
+		log.Fatalf("linux-guru: error creating client: %v", err)
+	}
+	return client
 }
